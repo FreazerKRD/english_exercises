df156f4f207c44a60f56def62cf169cbe59992bc
Freazer <freazer@yandex.ru>
2023-08-03 19:09:42 +0300

    Bugfix

--- app/keyboards/book_selection.py
+++ app/keyboards/book_selection.py
@@ -5,7 +5,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 def books_kb(posts: list, start_index: int, books_per_page: int) -> InlineKeyboardMarkup | int:
 
     # Set count of books per page and check if it's out of total books count
-    end_index = start_index + books_per_page - 1
+    end_index = start_index + books_per_page
     
     builder = InlineKeyboardBuilder()
     
@@ -15,32 +15,27 @@ def books_kb(posts: list, start_index: int, books_per_page: int) -> InlineKeyboa
             break
         else:
             # Create button for book and add to keyboard
-            builder.add(InlineKeyboardButton(
+            builder.row(InlineKeyboardButton(
                 text = str(posts[i]['file_name']).replace(".json", ""),
                 # Add prefix for filtering is callback query
                 callback_data='bs_' + str(posts[i]['id']))
             )
-            builder.adjust(1)
     
     # Show only actual buttons
-    if start_index <= books_per_page and books_per_page >= len(posts):
-        builder.add(InlineKeyboardButton(text="Отмена", callback_data="bs_cancel"))
-        builder.adjust(1)
+    if start_index < books_per_page and books_per_page >= len(posts):
+        builder.row(InlineKeyboardButton(text="Отмена", callback_data="bs_cancel"))
     
-    elif start_index <= books_per_page and end_index < len(posts):
-        builder.add(InlineKeyboardButton(text="Вперед", callback_data = "bs_forward"))
-        builder.add(InlineKeyboardButton(text="Отмена", callback_data="bs_cancel"))
-        builder.adjust(2)
+    elif start_index < books_per_page and end_index < len(posts):
+        builder.row(InlineKeyboardButton(text="Вперед", callback_data = "bs_forward"), 
+                    InlineKeyboardButton(text="Отмена", callback_data="bs_cancel"))
     
     elif start_index >= books_per_page and end_index >= len(posts):
-        builder.add(InlineKeyboardButton(text="Назад", callback_data="bs_back"))
-        builder.add(InlineKeyboardButton(text="Отмена", callback_data="bs_cancel"))
-        builder.adjust(2)
+        builder.row(InlineKeyboardButton(text="Назад", callback_data="bs_back"), 
+                    InlineKeyboardButton(text="Отмена", callback_data="bs_cancel"))
     
     else:
-        builder.add(InlineKeyboardButton(text="Вперед", callback_data = "bs_forward"))
-        builder.add(InlineKeyboardButton(text="Отмена", callback_data="bs_cancel"))
-        builder.add(InlineKeyboardButton(text="Назад", callback_data="bs_back"))
-        builder.adjust(3)
+        builder.row(InlineKeyboardButton(text="Вперед", callback_data = "bs_forward"), 
+                    InlineKeyboardButton(text="Отмена", callback_data="bs_cancel"), 
+                    InlineKeyboardButton(text="Назад", callback_data="bs_back"))
     
     return builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
